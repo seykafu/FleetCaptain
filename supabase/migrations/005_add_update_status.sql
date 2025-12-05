32f7a77c98ca42f67c33fdcc675165f9ca848e06
Kasey Fu <kaseykungfu@gmail.com>
2025-12-05 14:42:23 -0800

    Add UPDATE to maintenance_type enum as well

--- supabase/migrations/005_add_update_status.sql
+++ supabase/migrations/005_add_update_status.sql
@@ -1,3 +1,6 @@
 -- Add 'UPDATE' status to maintenance_status enum
 ALTER TYPE maintenance_status ADD VALUE IF NOT EXISTS 'UPDATE';
 
+-- Add 'UPDATE' type to maintenance_type enum
+ALTER TYPE maintenance_type ADD VALUE IF NOT EXISTS 'UPDATE';
+
